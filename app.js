d57617028a8678a6d1fa59c97aecb7bdfdd6109c
Clara <minneapolis-edu@users.noreply.github.com>
2017-12-13 19:50:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var passportConfig = require('./config/passport')(passport);
+
 var flash = require('express-flash');
 var mongoose = require('mongoose');
 var hbs = require('hbs');
 var helpers = require('./hbshelpers/helpers');
 var MongoDBStore = require('connect-mongodb-session')(session);
 //use mongodb environment variable
-var mongo_url=process.env.MONGO_URL1;
+var mongo_url=process.env.MONGO_URL;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongo_url, { useMongoClient: true})
@@ -24,7 +25,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
 
 // mongodb://armstrong:itrain@ds137826.mlab.com:37826/itrain
 // view engine setup
@@ -39,15 +39,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//stores sessions/cookies in DB
-var store = new MongoDBStore( {url : mongo_url, collection: 'users'}, function(err) {
+// stores sessions/cookies in DB
+var store = new MongoDBStore( {uri : mongo_url, collection: 'users'}, function(err) {
     if (err) {
         console.log('Error, can\'t connect to MongoDB to store session', err);
     }
 });
 
-/*stores cookies with unique ids and in conjunction with passport
-stores the user connected to that cookie*/
+//stores cookies with unique ids and in conjunction with passport
+//stores the user connected to that cookie
+
 app.use(session({
     secret: 'replace with long random string',
     resave: true,
@@ -55,21 +56,17 @@ app.use(session({
     store: store
 }));
 
-require('./config/passport')(passport);
-// passport.js module.export exports a function
-// that expects a passport object as an argument.
-// This require statement calls that function with the passport
-// object you required on line 10.
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', index);
 app.use('/users', users);
+app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
